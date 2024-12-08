71570cf6b08ccd8f53e00c5c61f9f6a2749a46c0
kriyank2798 <krishna.work2798@gmail.com>
2024-12-08 09:01:04 -0500

    8/12

--- src/gitpractice/GitDemoKrishna.java
+++ src/gitpractice/GitDemoKrishna.java
@@ -4,6 +4,7 @@ public class GitDemoKrishna {
     public static void main(String[] args) {
         System.out.println("My Name is Krishna p");
         System.out.println("I Love Java");
+
     }
 }
 
