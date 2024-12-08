66036eb574924335cab8cc548791a553ad1e379c
Mrindiamk <k4kenishalearning@gmail.com>
2024-12-08 13:45:53 +0000

    1234-Login_Kenisha

---
+++ src/gitpractice/GitDemoKenisha.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class GitDemoKenisha {
+    public static void main(String[] args) {
+        System.out.println("My name is Kenisha");
+        System.out.println("I love TestRail");
+    }
+}
