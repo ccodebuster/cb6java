37660cb52f4cd10d8d38715072255ab15b6787a0
avani244 <patelavani244@gmail.com>
2024-12-08 13:45:00 +0000

    Login001_Avani

---
+++ src/gotpractice/GitDemoAvani.java
@@ -0,0 +1,8 @@
+package gotpractice;
+
+public class GitDemoAvani {
+    public static void main(String[] args) {
+        System.out.println("My name is Avani");
+        System.out.println("I loce java");
+    }
+}
