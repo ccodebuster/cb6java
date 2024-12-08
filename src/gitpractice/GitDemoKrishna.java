895db7a800ae84291428818e406ad98fecfe7723
bhav3583 <bhavesh3583@yahoo.co.in>
2024-12-08 08:39:18 -0500

    8/12

---
+++ src/gitpractice/GitDemoKrishna.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class GitDemoKrishna {
+    public static void main(String[] args) {
+        System.out.println("My Name is Krishna");
+        System.out.println("I Love Java");
+    }
+}
