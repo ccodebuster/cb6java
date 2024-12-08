eef93100e4b075a6f6250e448d5bc15528dc8146
hemali1812 <hemali1812@gmail.com>
2024-12-08 13:45:06 +0000

    1234-Login_Hemali

---
+++ src/gitpractice/GitDemoHemali.java
@@ -0,0 +1,9 @@
+package gitpractice;
+
+public class GitDemoHemali {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Hemali");
+        System.out.println("I love java");
+    }
+}
