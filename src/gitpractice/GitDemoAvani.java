7b5d077d6683b5bdeb477c083d6f4e4d5eaf8ed4
avani244 <patelavani244@gmail.com>
2024-12-08 14:28:22 +0000

    updated

--- src/gitpractice/GitDemoAvani.java
+++ src/gitpractice/GitDemoAvani.java
@@ -1,8 +1,10 @@
-package gotpractice;
+package gitpractice;
 
 public class GitDemoAvani {
     public static void main(String[] args) {
         System.out.println("My name is Avani");
+        System.out.println("I loce java");
+        System.out.println("I loce java");
         System.out.println("I loce java");
     }
 }
