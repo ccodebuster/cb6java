0307c879e2d25dafb1471f7c83b5f45ada848d3a
Mrindiamk <k4kenishalearning@gmail.com>
2024-12-15 14:58:20 +0000

    my second push

--- src/gitpractice/GitDemoKenisha.java
+++ src/gitpractice/GitDemoKenisha.java
@@ -4,5 +4,12 @@ public class GitDemoKenisha {
     public static void main(String[] args) {
         System.out.println("My name is Kenisha");
         System.out.println("I love TestRail");
+        gitDemo();
+    }
+
+    public static void gitDemo(){
+        System.out.println("This is my Second Session of Git Demo");
+
+
     }
 }
