8e665972109390b1f6c87ee6db72909dd1a68b22
kr1709 <kirujan17@gmail.com>
2024-12-15 14:58:34 +0000

    My second push

--- src/gitpractice/GitDemoKirujan.java
+++ src/gitpractice/GitDemoKirujan.java
@@ -2,10 +2,16 @@ package gitpractice;
 
 public class GitDemoKirujan {
     public static void main(String[] args) {
-        System.out.println("My, Name is Kirujan");
+     /*   System.out.println("My, Name is Kirujan");
         System.out.println("Test");
         System.out.println("Hello");
         System.out.println("hi");
-        System.out.println("Hello");
+        System.out.println("Hello");*/
+        SecondSession();
+    }
+
+
+    public static void SecondSession (){
+        System.out.println("This is my secondl, Session of Git");
     }
 }
