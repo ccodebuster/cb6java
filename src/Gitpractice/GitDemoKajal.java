ecef7a71e576608b124e30cd27438f3d9ffc6fa8
kajal0414 <kajal0414@gmail.com>
2024-12-15 10:05:22 -0500

    This is my second session of git

--- src/Gitpractice/GitDemoKajal.java
+++ src/Gitpractice/GitDemoKajal.java
@@ -4,6 +4,10 @@ public class GitDemoKajal {
     public static void main(String[] args) {
         System.out.println("My name is Kajal");
         System.out.println("I love to play on java");
+        gitDemo();
+    }
 
+    public static void gitDemo(){
+        System.out.println("This is my second session of Git");
     }
 }
