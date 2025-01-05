5f7209fe1c6770919dd29c85017c4b4fb3865e45
kajal0414 <kajal0414@gmail.com>
2025-01-05 08:21:42 -0500

    trying it again

--- src/Gitpractice/GitDemoKajal.java
+++ src/Gitpractice/GitDemoKajal.java
@@ -3,6 +3,6 @@ package Gitpractice;
 public class GitDemoKajal {
     public static void main(String[] args) {
         System.out.println("learning is the key");
-
+        System.out.println("trying it again");
     }
 }
