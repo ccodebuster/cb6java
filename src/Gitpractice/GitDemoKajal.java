3e31ef8d90353f992d8f9e3cbb57cfee12c16ba6
kajal0414 <kajal0414@gmail.com>
2025-01-05 08:16:08 -0500

    learning is the key

--- src/Gitpractice/GitDemoKajal.java
+++ src/Gitpractice/GitDemoKajal.java
@@ -2,12 +2,7 @@ package Gitpractice;
 
 public class GitDemoKajal {
     public static void main(String[] args) {
-        System.out.println("My name is Kajal");
-        System.out.println("I love to play on java");
-        gitDemo();
-    }
+        System.out.println("learning is the key");
 
-    public static void gitDemo(){
-        System.out.println("This is my second session of Git");
     }
 }
